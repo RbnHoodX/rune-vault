fe6621460c47f27a8a0135a02c30b8637fdf9790
RbnHoodX <rbnhoodx@gmail.com>
2026-03-13 19:23:10 +0200

    Refactor Rune to use @property for id and name

--- rune.py
+++ rune.py
@@ -2,17 +2,29 @@ class Rune:
     """A single rune in the vault."""
 
     def __init__(self, name=""):
-        self.id = 0
-        self.name = name
+        self._id = 0
+        self._name = name
         self.stage = "blank"
 
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, value):
+        self._id = value
+
+    @property
+    def name(self):
+        return self._name
+
     def __repr__(self):
-        return f"Rune(id={self.id}, name={self.name!r})"
+        return f"Rune(id={self._id}, name={self._name!r})"
 
     def __eq__(self, other):
         if not isinstance(other, Rune):
             return NotImplemented
-        return self.id == other.id
+        return self._id == other._id
 
     def __hash__(self):
-        return hash(self.id)
+        return hash(self._id)
